e3b8f81ec1a7c781f857af60a78b8bd303290a21
Votre Nom Ici <antoine.2vey@gmail.com>
2016-03-20 18:22:39 +0100

    Delete archive.php (not used anymore)

--- SiteGrece/wp-content/themes/grece/archive.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-	<div class="main">
-		<?php query_posts('posts_per_page=10'); ?>
-<?php get_template_part('loop'); ?>
-	</div>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
